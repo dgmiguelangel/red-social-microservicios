4a3b89f2a377c7a69673c7d45a7500d24b966598
Miguel <dgmiguelangel@gmail.com>
2026-02-13 13:01:07 -0300

    update crud to microservice

--- store/remote.js
+++ store/remote.js
@@ -7,9 +7,39 @@ function createRemoteDB(host, port) {
         return req('GET', table);
     }
 
+    function get(table, id) {
+        return req('GET', table, id);
+    }
+
+    function insert(table, data) {
+		return req('POST', table, data);
+	}
+
+	function update(table, data) {
+		return req('PUT', table, data);
+	}
+
+	function upsert(table, data) {
+		if (data.id) {
+			return update(table, data);
+		}
+
+		return insert(table, data);
+	}
+
+	function query(table, query, join) {
+		return req('POST', table + '/query', { query, join });
+	}
+
     function req(method, table, data) {
         let url = URL + '/' + table;
         body = '';
+
+        if (method === 'GET' && data) {
+			url += '/'+ data;
+		} else if (data) {
+			body = JSON.stringify(data);
+		}
 
         return new Promise((resolve, reject) => {
             request({
@@ -26,6 +56,7 @@ function createRemoteDB(host, port) {
                 }
 
                 const resp = JSON.parse(body);
+
                 return resolve(resp.body);
             })
         })
@@ -33,6 +64,10 @@ function createRemoteDB(host, port) {
 
     return {
         list,
+        get,
+        insert,
+        update,
+        query
     }
 }
 
