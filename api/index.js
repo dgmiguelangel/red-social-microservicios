3af09cf13f332e2cfec74b4e171752658e54df66
Miguel <dgmiguelangel@gmail.com>
2026-02-13 17:18:41 -0300

    add post as microservice

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const swaggerDocument = require('./swagger.json');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
 
 const errors = require('../network/errors');
 
@@ -16,7 +15,6 @@ app.use(bodyParser.json()); // para que el servidor pueda entender el formato js
 // ROUTER
 app.use('/api/user', user); // cada vez que se haga una petición a /api/user, se va a ejecutar el router que se encuentra en ./components/user/network.js
 app.use('/api/auth', auth);
-app.use('/api/post', post);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errors); // middleware para manejar los errores
