bf41f16aa71850f22a2a76cf727706b10f9da3fa
Miguel <dgmiguelangel@gmail.com>
2026-02-15 09:52:39 -0300

    config redis

--- store/redis.js
+++ store/redis.js
@@ -7,35 +7,34 @@ const client = redis.createClient({
     url: `redis://${config.redis_local.username}:${config.redis_local.password}@${config.redis_local.host}:${config.redis_local.port}`
 });
 
-client.on('error', (err) => console.error('Redis Client Error: ', err));
-
-
-let connected = false;
-
-async function connectIfNeeded(){
-    if(!connected){
-        await client.connect();
-        connected = true;
-    }
-}
+(async () => {
+  await client.connect();
+  console.log('Conectado a REDIS');
+})();
 
 async function list(table){
-    await connectIfNeeded();
-    const data = await client.get(table);
-    return data ? JSON.stringify(data) : null;
+    const values = await client.get(table);
+    return values ? JSON.parse(values) : null;
+}
+
+async function get(table, id) {
+    const value = await client.get(`${table}_${id}`);
+    return JSON.parse(value);
 }
 
 async function upsert(table, data){
     let key = table;
+
     if(data && data.id){
         key = `${table}_${data.id}`;
     }
-    await connectIfNeeded();
+
     await client.setEx(key, 10, JSON.stringify(data));
     return true;
 }
 
 module.exports = {
     list,
+    get,
     upsert
 };
